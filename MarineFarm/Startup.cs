3ed82f756be3e65135d3cd8a4c10e347091321d1
tjodalv2k <svennsorensen@gmail.com>
2019-09-07 21:54:32 +0200

    Removed unused controller, added swagger to startup

--- MarineFarm/Startup.cs
+++ MarineFarm/Startup.cs
@@ -27,7 +27,7 @@ namespace MarineFarm
 
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "MarineFarm", Version = "v1" });
             });
         }
 
