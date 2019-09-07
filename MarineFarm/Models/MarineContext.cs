69d102bcde65903bc044dcc16a3e7fb736e1b6b6
tjodalv2k <svennsorensen@gmail.com>
2019-09-07 15:46:36 +0200

    init commit

---
+++ MarineFarm/Models/MarineContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MarineFarm.Models
+{
+    public class MarineContext:DbContext
+    {
+        public MarineContext(DbContextOptions<MarineContext> options): base(options)
+        {
+
+        }
+    }
+}
